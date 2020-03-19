495a43f43f80f67de9d17df9b3f78b1e5ffd20d8
justinlewis <j.mapping@gmail.com>
2020-03-18 18:07:40 -0600

    data update march 18 2020

--- data/colorado.js
+++ data/colorado.js
@@ -1,1 +1,67 @@
-var coloradocovid = {"presumed": {"Adams": 6, "Arapahoe": 15, "Boulder": 3, "Clear Creek": 1, "Denver": 25, "Douglas": 8, "Eagle": 24, "El Paso": 4, "Garfield": 1, "Gunnison": 8, "Jefferson": 12, "Larimer": 1, "Mesa": 1, "Pitkin": 11, "Pueblo": 1, "Summit": 1, "Weld": 5, "Routt": 1}, "last_update": "2020-03-15 17:18:13.625569", "last_source_update": "March 15, 1:45 p.m.", "age_breakdown": [{"category": "80s", "count": 0}, {"category": "Teenage", "count": 7}, {"category": "20s", "count": 13}, {"category": "30s", "count": 26}, {"category": "50s", "count": 26}, {"category": "60s", "count": 16}, {"category": "70s", "count": 14}, {"category": "80s", "count": 5}]}
+var coloradocovid = {
+  "presumed": {
+    "Adams": 8,
+    "Arapahoe": 18,
+    "Boulder": 8,
+    "Clear Creek": 2,
+    "Denver": 38,
+    "Douglas": 10,
+    "Eagle": 34,
+    "El Paso": 6,
+    "Garfield": 2,
+    "Gunnison": 11,
+    "Jefferson": 17,
+    "Larimer": 2,
+    "Mesa": 1,
+    "Pitkin": 11,
+    "Pueblo": 1,
+    "Summit": 3,
+    "Weld": 5,
+    "Routt": 1,
+    "Route": 2,
+  },
+  "last_update": "2020-03-15",
+  "last_source_update": "March 18, 6:00 p.m.",
+  "age_breakdown": [
+    {
+      "category": "80s",
+      "count": 0
+    },
+    {
+      "category": "10s",
+      "count": 7
+    },
+    {
+      "category": "20s",
+      "count": 20
+    },
+    {
+      "category": "30s",
+      "count": 39
+    },
+    {
+      "category": "40s",
+      "count": 35
+    },
+    {
+      "category": "50s",
+      "count": 33
+    },
+    {
+      "category": "60s",
+      "count": 23
+    },
+    {
+      "category": "70s",
+      "count": 15
+    },
+    {
+      "category": "80s",
+      "count": 6
+    },
+    {
+      "category": "Unknown",
+      "count": 5
+    }
+  ]
+}
