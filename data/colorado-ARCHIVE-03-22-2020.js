9f4f1a4f7c783429a33f11cf65a1e970783badbc
justinlewis <j.mapping@gmail.com>
2020-03-23 16:33:58 -0600

    update 03 -23-2020

--- data/colorado-ARCHIVE-03-22-2020.js
+++ data/colorado-ARCHIVE-03-22-2020.js
@@ -1,33 +1,31 @@
 var coloradocovid = {
   "presumed": {
-    "Adams": 25,
-    "Arapahoe": 58,
-    "Boulder": 39,
-    "Broomfield": 2,
+    "Adams": 18,
+    "Arapahoe": 45,
+    "Boulder": 37,
+    "Broomfield": 1,
     "Chaffee": 3,
     "Clear Creek": 3,
     "Crowley": 1,
-    "Denver": 148,
-    "Douglas": 48,
-    "Eagle": 92,
-    "El Paso": 69,
-    "Elbert": 1,
+    "Denver": 125,
+    "Douglas": 33,
+    "Eagle": 80,
+    "El Paso": 51,
+    "Elbert": 3,
     "Garfield": 8,
     "Grand": 2,
-    "Gunnison": 28,
+    "Gunnison": 22,
     "Hinsdale": 1,
-    "Jefferson": 55,
-    "Larimer": 34,
-    "Logan": 2,
+    "Jefferson": 51,
+    "Larimer": 32,
     "Mesa": 2,
     "Montrose": 1,
-    "Morgan": 3,
+    "Morgan": 2,
     "Park": 1,
-    "Pitkin": 18,
+    "Pitkin": 16,
     "Pueblo": 3,
-    "Summit": 8,
-    "Teller": 3,
-    "Weld": 50,
+    "Summit": 7,
+    "Weld": 5,
     "Routt": 3,
     "San Miguel": 1,
     "Summit": 7,
@@ -39,39 +37,39 @@ var coloradocovid = {
   "age_breakdown": [
     {
       "category": "0-9",
-      "count": 7
+      "count": 6
     },
     {
       "category": "10-19",
-      "count": 22
+      "count": 18
     },
     {
       "category": "20-29",
-      "count": 101
+      "count": 83
     },
     {
       "category": "30-39",
-      "count": 122
-    },
-    {
-      "category": "40-49",
-      "count": 144
-    },
-    {
-      "category": "50-59",
-      "count": 115
-    },
-    {
-      "category": "60-69",
       "count": 101
     },
     {
+      "category": "40-49",
+      "count": 118
+    },
+    {
+      "category": "50-59",
+      "count": 95
+    },
+    {
+      "category": "60-69",
+      "count": 83
+    },
+    {
       "category": "70-79",
-      "count": 79
+      "count": 59
     },
     {
       "category": "80+",
-      "count": 36
+      "count": 30
     }
   ]
 }
